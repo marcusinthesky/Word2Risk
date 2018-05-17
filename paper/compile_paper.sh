10af61af1599361cbadf629aa4e9c3c8b6db8fcc
Marcus Gawronsky <gwrmar002@myuct.ac.za>
2018-05-17 11:34:04 +0200

    Fixes to citations, submitted Literature Review.

--- paper/compile_paper.sh
+++ paper/compile_paper.sh
@@ -1,4 +1,3 @@
 jupyter nbconvert --to markdown --template=hidecode.tpl Proposal.ipynb
 
 pandoc -s Proposal.md --filter pandoc-citeproc --no-highlight --listings --number-sections -o Proposal.pdf
-
